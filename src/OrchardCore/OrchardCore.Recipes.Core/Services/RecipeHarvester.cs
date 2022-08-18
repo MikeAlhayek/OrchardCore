83c3379acffdf43ae7ebc7986e59391a960ec16b
Mike Alhayek <mike@crestapps.com>
2022-08-18 11:27:50 -0700

    update the recipe harvester

--- src/OrchardCore/OrchardCore.Recipes.Core/Services/RecipeHarvester.cs
+++ src/OrchardCore/OrchardCore.Recipes.Core/Services/RecipeHarvester.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using OrchardCore.Environment.Extensions;
+using OrchardCore.Environment.Shell;
 using OrchardCore.Modules;
 using OrchardCore.Recipes.Models;
 
@@ -17,16 +19,19 @@ namespace OrchardCore.Recipes.Services
         private readonly IExtensionManager _extensionManager;
         private readonly IHostEnvironment _hostingEnvironment;
         private readonly ILogger _logger;
+        private readonly ShellSettings _shellSettings;
 
         public RecipeHarvester(
             IRecipeReader recipeReader,
             IExtensionManager extensionManager,
             IHostEnvironment hostingEnvironment,
+            ShellSettings shellSettings,
             ILogger<RecipeHarvester> logger)
         {
             _recipeReader = recipeReader;
             _extensionManager = extensionManager;
             _hostingEnvironment = hostingEnvironment;
+            _shellSettings = shellSettings;
             _logger = logger;
         }
 
@@ -48,14 +53,23 @@ namespace OrchardCore.Recipes.Services
         /// <returns>The list of <see cref="RecipeDescriptor"/> instances.</returns>
         protected Task<IEnumerable<RecipeDescriptor>> HarvestRecipesAsync(string path)
         {
-            var recipeDescriptors = new List<RecipeDescriptor>();
+            IEnumerable<IFileInfo> files;
 
-            var recipeFiles = _hostingEnvironment.ContentRootFileProvider.GetDirectoryContents(path)
-                .Where(x => !x.IsDirectory && x.Name.EndsWith(".recipe.json", StringComparison.Ordinal));
+            if (_shellSettings.IsDefaultShell())
+            {
+                files = _hostingEnvironment.ContentRootFileProvider.GetDirectoryContents(path);
+            }
+            else
+            {
+                files = _extensionManager.GetExtensions()
+                    .SelectMany(x => _hostingEnvironment.ContentRootFileProvider.GetDirectoryContents(Path.Combine(path, x.SubPath)))
+                    .Where(x => !x.IsDirectory && x.Name.EndsWith(".recipe.json", StringComparison.Ordinal));
+            }
 
-            recipeDescriptors.AddRange(recipeFiles.Select(recipeFile => _recipeReader.GetRecipeDescriptor(path, recipeFile, _hostingEnvironment.ContentRootFileProvider).Result));
+            var recipeFiles = files.Where(x => !x.IsDirectory && x.Name.EndsWith(".recipe.json", StringComparison.Ordinal))
+                                   .Select(recipeFile => _recipeReader.GetRecipeDescriptor(path, recipeFile, _hostingEnvironment.ContentRootFileProvider).Result);
 
-            return Task.FromResult<IEnumerable<RecipeDescriptor>>(recipeDescriptors);
+            return Task.FromResult(recipeFiles);
         }
     }
 }
